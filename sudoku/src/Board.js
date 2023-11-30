e6958192c78a01f14b362e19243fe1a5832ef946
Nisa Meshal <nisameshal@gmail.com>
2023-11-29 19:14:44 -0800

    Properly functioning board

--- sudoku/src/Board.js
+++ sudoku/src/Board.js
@@ -2,29 +2,34 @@ import './Board.css';
 import { useState } from "react";
 
 function Board() {
-    const [board, setBoard] = useState(Array.from({length: 9},()=> Array.from({length: 9}, () => 0)));
+    let[board, setBoard] = useState(Array.from({length: 9}, ()=> Array.from({length: 9}, () => 0)));
 
-    const handleBoxChange = (row, col, e) => {
+    const handleBoxChange = (e, row, col) => {
         let newBoard = [...board]; 
         newBoard[row][col] = e.target.value; 
         setBoard(newBoard); 
+        console.log(`row${row} col${col}`);
     };
 
     return(
         <>
             <table id="board">
                 <tbody>
-                    {
-                        board.map( function(row) {
-                            return <tr>
-                            {
-                                row.map( function(col) {
-                                    return <td><input type="number" value={col} onChange={e => handleBoxChange(row, col, e)}/></td>
-                                })
-                            }
-                            </tr>
-                        })
-                    }
+                {
+                    board.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                        {
+                            row.map((col, colIndex) => (
+                                <td key={colIndex}>
+                                <input
+                                    type="number"
+                                    onChange={e => handleBoxChange(e, rowIndex, colIndex)}
+                                />
+                                </td>
+                            ))}
+                        </tr>
+                    ))
+                }
                 </tbody>
             </table>
         </>
