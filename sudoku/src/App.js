8589a43391ccac532d2ab9e6250a5661948d6118
Nisa Meshal <nisameshal@gmail.com>
2023-12-10 23:43:42 -0800

    board logic

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -1,8 +1,34 @@
 import './css/App.css';
 import Board from './Board'; 
 import Instructions from './Instructions';
+import { useState } from 'react';
+
+let board_sol = [
+  [8,6,5,1,2,9,7,4,3],
+  [7,3,9,8,6,4,5,1,2],
+  [1,4,2,5,7,3,9,6,8], 
+  [3,2,8,9,1,7,6,5,4],
+  [5,1,4,6,3,8,2,9,7], 
+  [6,9,7,4,5,2,3,8,1], 
+  [9,5,3,7,4,1,8,2,6], 
+  [4,7,6,2,8,5,1,3,9],
+  [2,8,1,3,9,6,4,7,5]
+]
+
+let play_board = [
+  ['','',5,1,'',9,7,'',''],
+  ['',3,9,'',6,'',5,1,''],
+  ['',4,'','',7,'','',6,''], 
+  ['','',8,'','','',6,'',4],
+  ['','','',6,'',8,'',9,''], 
+  ['',9,'',4,5,2,3,'',1], 
+  ['',5,'','',4,'','',2,''], 
+  ['',7,'','',8,'','',3,''],
+  ['',8,1,3,9,6,4,7,'']  
+]
 
 function App() {
+  let[board, setBoard] = useState(play_board); 
 
   return (
     <div className="App">
@@ -15,15 +41,15 @@ function App() {
       <body>
         <h1>Sudoku</h1>
         <div id='page-container'>
-          <h3 id='instruction-title'>How to play</h3>
+          <h2 id='instruction-title'>How to play</h2>
           <div id='instruction-container'>
             <Instructions />
           </div>
           <button id='start-button'>Start Game</button>
           <div id='board-container'>
-            <Board/>
+            <Board board={board} setBoard={setBoard}/>
           </div>
-          <h3 id='history-title'>Game History</h3>
+          <h2 id='history-title'>Game History</h2>
           <div id='record-container'></div>
         </div>
       </body>
--- sudoku/src/Board.js
+++ sudoku/src/Board.js
@@ -1,24 +1,13 @@
 import './css/Board.css'; 
 import { useState } from "react";
 
-let board1 = [
-    [8,6,5,1,2,9,7,4,3],
-    [7,3,9,8,6,4,5,1,2],
-    [1,4,2,5,7,3,9,6,8], 
-    [3,2,8,9,1,7,6,5,4],
-    [5,1,4,6,3,8,2,9,7], 
-    [6,9,7,4,5,2,3,8,1], 
-    [9,5,3,7,4,1,8,2,6], 
-    [4,7,6,2,8,5,1,3,9],
-    [2,8,1,3,9,6,4,7,5]
-]
-
-function Board() {
-    let[board, setBoard] = useState(Array.from({ length: 9}, () => Array.from({ length: 9}, () => 0)));
+function Board({board, setBoard}) {
 
     const handleBoxChange = (e, row, col) => {
         let newBoard = [...board]; 
-        newBoard[row][col] = e.target.value; 
+        if(e.target.value <= 9 && e.target.value >= 1) {
+            newBoard[row][col] = e.target.value; 
+        }
         setBoard(newBoard); 
         console.log(board); 
     };
@@ -32,13 +21,15 @@ function Board() {
                         <tr key={rowIndex}>
                         {
                             row.map((col, colIndex) => (
-                                <td key={colIndex}>
+                                <td key={colIndex}> 
                                 <input
                                     type="number"
                                     onChange={e => handleBoxChange(e, rowIndex, colIndex)}
+                                    value={board[rowIndex][colIndex]}
                                 />
                                 </td>
-                            ))}
+                            ))
+                        }
                         </tr>
                     ))
                 }
